43f47619bac9677b31df064058806ac09f9c99e2
eric_yu <senekis@sina.com>
2020-09-25 15:54:27 +0800

    add 62

--- src/main/java/practice/leecode/Arrays/UniquePaths_62.java
+++ src/main/java/practice/leecode/Arrays/UniquePaths_62.java
@@ -1,2 +1,37 @@
-package practice.leecode.Arrays;public class UniquePaths_62 {
+package practice.leecode.Arrays;
+
+import java.util.Arrays;
+
+public class UniquePaths_62 {
+
+    public int uniquePaths(int m, int n) {
+        int[] nums = new int[n];
+        Arrays.fill(nums, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                nums[j] += nums[j - 1];
+            }
+        }
+        return nums[n - 1];
+    }
+
+//    方法1:
+//    public int uniquePaths(int m, int n) {
+//        int[][] matrix = new int[m][n];
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < n; j++) {
+//                if (i == 0 && j == 0) {
+//                    matrix[i][j] = 1;
+//                } else if (i == 0) {
+//                    matrix[i][j] = matrix[i][j - 1];
+//                } else if (j == 0) {
+//                    matrix[i][j] = matrix[i - 1][j];
+//                } else {
+//                    matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1];
+//                }
+//            }
+//        }
+//        return matrix[m - 1][n - 1];
+//    }
+
 }
--- src/test/java/practice/leecode/Arrays/UniquePaths_62Test.java
+++ src/test/java/practice/leecode/Arrays/UniquePaths_62Test.java
@@ -1,4 +1,21 @@
+package practice.leecode.Arrays;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class UniquePaths_62Test {
-  
+
+    @Test
+    public void uniquePaths_1() {
+        UniquePaths_62 uniquePaths_62 = new UniquePaths_62();
+        assertEquals(3, uniquePaths_62.uniquePaths(3,2));
+    }
+
+    @Test
+    public void uniquePaths_2() {
+        UniquePaths_62 uniquePaths_62 = new UniquePaths_62();
+        assertEquals(28, uniquePaths_62.uniquePaths(7,3));
+    }
+
 }
