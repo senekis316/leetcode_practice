81e401053fc5c2e469eb0d5b20a366067232bf63
eric_yu <senekis@sina.com>
2021-03-31 18:30:05 +0800

    add 90

--- src/main/java/practice/leecode/Arrays/SubsetsWithDup_90.java
+++ src/main/java/practice/leecode/Arrays/SubsetsWithDup_90.java
@@ -2,29 +2,24 @@ package practice.leecode.Arrays;
 
 import java.util.*;
 
+
 public class SubsetsWithDup_90 {
 
-    private List<List<Integer>> res;
-    private Map<String, List<Integer>> map;
-
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        map = new HashMap<>();
-        res = new ArrayList<>();
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
         res.add(new ArrayList<>());
+        Set<List<Integer>> set = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
-            String sub = String.valueOf(nums[i]);
-            List<Integer> sublist = new ArrayList<>();
-            sublist.add(nums[i]);
-            if (map.get(sub) == null) {
-                map.put(sub, new ArrayList<>(sublist));
-                res.add(map.get(sub));
-            }
-            for (int j = i + 1; j < nums.length; j++) {
-                sub = sub + "|" + nums[j];
-                sublist.add(nums[j]);
-                if (map.get(sub) == null) {
-                    map.put(sub, new ArrayList<>(sublist));
-                    res.add(map.get(sub));
+            int size = res.size();
+            for (int j = 0; j < size; j++) {
+                List<Integer> sub = new ArrayList<>(res.get(j));
+                sub.add(nums[i]);
+                if (!set.contains(sub)) {
+                    set.add(sub);
+                    res.add(new ArrayList<>(sub));
+                } else {
+                    sub.remove(sub.size() - 1);
                 }
             }
         }
--- src/test/java/practice/leecode/Arrays/SubsetsWithDup_90Test.java
+++ src/test/java/practice/leecode/Arrays/SubsetsWithDup_90Test.java
@@ -11,39 +11,33 @@ public class SubsetsWithDup_90Test {
 
     @Test
     public void subsetsWithDup_1() {
+
+        List<List<Integer>> expect = new ArrayList<>();
+        expect.add(List.of());
+        expect.add(List.of(1));
+        expect.add(List.of(2));
+        expect.add(List.of(1,2));
+        expect.add(List.of(2,2));
+        expect.add(List.of(1,2,2));
+
         SubsetsWithDup_90 subsetsWithDup_90 = new SubsetsWithDup_90();
-        List<List<Integer>> res = subsetsWithDup_90.subsetsWithDup(new int[]{1,2,2});
+        List<List<Integer>> result = subsetsWithDup_90.subsetsWithDup(new int[]{1,2,2});
 
-        assertEquals(0, res.get(0).size());
-
-        assertEquals(1, res.get(1).size());
-        assertEquals(1, res.get(1).get(0).intValue());
-
-        assertEquals(2, res.get(2).size());
-        assertEquals(1, res.get(2).get(0).intValue());
-        assertEquals(2, res.get(2).get(1).intValue());
-
-        assertEquals(3, res.get(3).size());
-        assertEquals(1, res.get(3).get(0).intValue());
-        assertEquals(2, res.get(3).get(1).intValue());
-        assertEquals(2, res.get(3).get(2).intValue());
-
-        assertEquals(1, res.get(4).size());
-        assertEquals(2, res.get(4).get(0).intValue());
-
-        assertEquals(2, res.get(5).size());
-        assertEquals(2, res.get(5).get(0).intValue());
-        assertEquals(2, res.get(5).get(1).intValue());
+        assertEquals(expect, result);
 
     }
 
     @Test
     public void subsetsWithDup_2() {
+
+        List<List<Integer>> expect = new ArrayList<>();
+        expect.add(List.of());
+        expect.add(List.of(0));
+
         SubsetsWithDup_90 subsetsWithDup_90 = new SubsetsWithDup_90();
-        List<List<Integer>> res = subsetsWithDup_90.subsetsWithDup(new int[]{0});
-        assertEquals(0, res.get(0).size());
-        assertEquals(1, res.get(1).size());
-        assertEquals(0, res.get(1).get(0).intValue());
+        List<List<Integer>> result = subsetsWithDup_90.subsetsWithDup(new int[]{0});
+        assertEquals(expect, result);
+
     }
 
 }
