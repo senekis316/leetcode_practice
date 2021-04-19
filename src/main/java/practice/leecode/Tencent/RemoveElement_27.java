e6b7d9daae238069bd62188ed0dca74ff98ef5ce
eric_yu <senekis@sina.com>
2021-04-19 11:16:55 +0800

    modify 27

--- src/main/java/practice/leecode/Tencent/RemoveElement_27.java
+++ src/main/java/practice/leecode/Tencent/RemoveElement_27.java
@@ -4,7 +4,7 @@ public class RemoveElement_27 {
 
     public int removeElement(int[] nums, int val) {
         int remove = 0;
-        for (int i = 0; i<nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] == val) {
                 remove++;
             } else {
@@ -13,6 +13,5 @@ public class RemoveElement_27 {
         }
         return nums.length - remove;
     }
-
 
 }
