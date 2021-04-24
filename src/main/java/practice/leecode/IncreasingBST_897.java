65d6c6ae73ec0057375e64d5f5ee84468380ada5
eric_yu <senekis@sina.com>
2021-04-25 01:36:47 +0800

    modify 897

--- src/main/java/practice/leecode/IncreasingBST_897.java
+++ src/main/java/practice/leecode/IncreasingBST_897.java
@@ -7,24 +7,42 @@ import java.util.List;
 
 public class IncreasingBST_897 {
 
-    TreeNode first;
+    private TreeNode head;
 
     public TreeNode increasingBST(TreeNode root) {
-        TreeNode head = new TreeNode(0);
-        first = head;
+        TreeNode prev = new TreeNode(-1);
+        head = prev;
         middleOrder(root);
-        return head.right;
+        return prev.right;
     }
 
     private void middleOrder(TreeNode root) {
-        if (root == null) {
-            return;
-        }
+        if (root == null) return;
         middleOrder(root.left);
-        first.right = new TreeNode(root.val);
-        first = first.right;
+        root.left = null;
+        head.right = root;
+        head = head.right;
         middleOrder(root.right);
-        return;
     }
+
+//    TreeNode first;
+//
+//    public TreeNode increasingBST(TreeNode root) {
+//        TreeNode head = new TreeNode(0);
+//        first = head;
+//        middleOrder(root);
+//        return head.right;
+//    }
+//
+//    private void middleOrder(TreeNode root) {
+//        if (root == null) {
+//            return;
+//        }
+//        middleOrder(root.left);
+//        first.right = new TreeNode(root.val);
+//        first = first.right;
+//        middleOrder(root.right);
+//        return;
+//    }
 
 }
