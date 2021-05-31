de550b1fa94e87a79c63df2fe31cf66f2345b29e
Eric Yu <senekis@sina.com>
2021-05-31 11:44:14 +0800

    add 342

--- src/main/java/practice/leecode/Bits/IsPowerOfFour_342.java
+++ src/main/java/practice/leecode/Bits/IsPowerOfFour_342.java
@@ -3,7 +3,7 @@ package practice.leecode.Bits;
 public class IsPowerOfFour_342 {
 
     public boolean isPowerOfFour(int num) {
-        if (num < 0 || (num & (num - 1)) != 0) {
+        if (num <= 0 || (num & (num - 1)) != 0) {
             return false;
         }
         return (num & 0x55555555) == num;
--- src/test/java/practice/leecode/Bits/IsPowerOfFour_342Test.java
+++ src/test/java/practice/leecode/Bits/IsPowerOfFour_342Test.java
@@ -7,9 +7,16 @@ import static org.junit.Assert.*;
 public class IsPowerOfFour_342Test {
 
     @Test
-    public void isPowerOfFour() {
+    public void isPowerOfFour_1() {
         IsPowerOfFour_342 isPowerOfFour_342 = new IsPowerOfFour_342();
         assertEquals(false, isPowerOfFour_342.isPowerOfFour(5));
         assertEquals(true, isPowerOfFour_342.isPowerOfFour(16));
     }
+
+    @Test
+    public void isPowerOfFour_2() {
+        IsPowerOfFour_342 isPowerOfFour_342 = new IsPowerOfFour_342();
+        assertEquals(false, isPowerOfFour_342.isPowerOfFour(0));
+    }
+
 }
