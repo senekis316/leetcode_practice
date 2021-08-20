13379ac1c41cfc6520b7f4c1b0a1f76f32ba1ce5
eric yu <yuf@wicresoft.com>
2021-08-20 12:13:21 +0800

    modify 541

--- src/main/java/practice/leecode/Arrays/ReverseStr_541.java
+++ src/main/java/practice/leecode/Arrays/ReverseStr_541.java
@@ -3,7 +3,7 @@ package practice.leecode.Arrays;
 public class ReverseStr_541 {
 
     public String reverseStr(String s, int k) {
-        char[] chars = new char[s.length()];
+        char[] chars = s.toCharArray();
         for (int i = 0; i < s.length(); i += 2 * k) {
             int l = i;
             int r = i + k - 1;
@@ -11,11 +11,9 @@ public class ReverseStr_541 {
                 r = s.length() - 1;
             }
             while (l <= r) {
-                chars[l] = s.charAt(r);
-                chars[r--] = s.charAt(l++);
-            }
-            for (int j = i + k;  j < s.length() && j < i + 2 * k; j++) {
-                chars[j] = s.charAt(j);
+                char tmp = chars[l];
+                chars[l++] = chars[r];
+                chars[r--] = tmp;
             }
         }
         return new String(chars);
