2ac3d5ec26e8e77d77b57c39672f88aab2628062
eric yu <yuf@wicresoft.com>
2021-08-23 10:15:53 +0800

    add 541

--- src/test/java/practice/leecode/Arrays/ReverseStr_541Test.java
+++ src/test/java/practice/leecode/Arrays/ReverseStr_541Test.java
@@ -2,12 +2,15 @@ package practice.leecode.Arrays;
 
 import org.junit.Test;
 
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 public class ReverseStr_541Test {
 
     @Test
     public void reverseStr_1() {
+        System.out.println(new Date().getTime());
         ReverseStr_541 reverseStr_541 = new ReverseStr_541();
         assertEquals("bacdfeg", reverseStr_541.reverseStr("abcdefg", 2));
     }
