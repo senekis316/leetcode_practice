bf797035dca266fb0f0b970ba5d550ebd2646737
eric yu <yuf@wicresoft.com>
2021-10-18 10:10:23 +0800

    modify 476

--- src/main/java/practice/leecode/Bits/FindComplement_476.java
+++ src/main/java/practice/leecode/Bits/FindComplement_476.java
@@ -5,9 +5,9 @@ public class FindComplement_476 {
     public int findComplement(int num) {
         int num1 = num;
         int num2 = 0;
-        while(num1 > 0) {
+        while (num1 > 0) {
             num1 >>= 1;
-            num2 = num2 == 0 ? num2 + 1 : (num2 << 1) + 1;
+            num2 = (num2 << 1) + 1;
         }
         return num ^ num2;
     }
