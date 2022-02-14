28df342bdb1d28f6c90789c1310299ab4f827627
eric yu <yuf@wicresoft.com>
2022-02-14 12:16:13 +0800

    netty http server simple demo

---
+++ src/main/java/practice/Java/study/nio/netty/NettyHttpServer.java
@@ -0,0 +1,79 @@
+package practice.Java.study.nio.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpObject;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.util.CharsetUtil;
+import org.springframework.http.MediaType;
+
+public class NettyHttpServer {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+            serverBootstrap.group(bossGroup, workGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 128)
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        nioSocketChannel.pipeline()
+                            .addLast(new HttpServerCodec())
+                            .addLast(new NettyHttpServerHandler());
+                    }
+                });
+
+            ChannelFuture channelFuture = serverBootstrap.bind(6889).sync();
+            channelFuture.channel().closeFuture().sync();
+
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+
+    }
+
+    public static class NettyHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+
+        @Override
+        protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject) throws Exception {
+
+            if (httpObject instanceof HttpRequest) {
+
+                ByteBuf content = Unpooled.copiedBuffer("hello, client", CharsetUtil.UTF_8);
+
+                FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
+                response.headers().set(HttpHeaderNames.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+
+                channelHandlerContext.writeAndFlush(response);
+            }
+        }
+    }
+
+
+
+}
